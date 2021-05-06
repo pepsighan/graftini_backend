157175bed89b1afa309f83ca58e2b6f58e853e4b
Sharad Chand <pepsighan@sharadchand.com>
2021-05-06 19:58:45 +0545

    Add require user from context func

--- auth/auth.go
+++ auth/auth.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -11,23 +12,29 @@ import (
 	"github.com/pepsighan/nocodepress_backend/ent/user"
 )
 
+var ErrUnauthorizedAccess = errors.New("unauthorized access")
+
 type AuthContext struct {
 	echo.Context
 	token string
 }
 
-// User gets the user if a valid authentication header is present in the request.
+// User gets the user if a valid authentication header is present in the request. If there is no token in the
+// request then it deems to be an unauthenticated request. If there is it treats the request likewise.
 func (a *AuthContext) User(ctx context.Context, entClient *ent.Client, firebaseAuth *auth.Client) (*ent.User, error) {
 	if a.token == "" {
-		// There is no user in the request.
+		// There is no user in the request. If there is no user for the user, do nothing.
 		return nil, nil
 	}
 
 	token, err := firebaseAuth.VerifyIDToken(ctx, a.token)
 	if err != nil {
-		return nil, fmt.Errorf("could not get user from auth: %w", err)
+		// The request has an authorized token and if the token verification fails, then the user is unauthorized.
+		return nil, fmt.Errorf("could not get user from auth: %w due to: %v", ErrUnauthorizedAccess, err)
 	}
 
+	// The following errors in the subsequent code are not actually unauthorized access errors because the following
+	// errors are caused due to some other failures.
 	user, err := entClient.User.Query().Where(user.FirebaseUIDEQ(token.UID)).First(ctx)
 	if err != nil && !ent.IsNotFound(err) {
 		return nil, fmt.Errorf("could not get user from database: %w", err)
@@ -57,14 +64,29 @@ func (a *AuthContext) User(ctx context.Context, entClient *ent.Client, firebaseA
 	return user, nil
 }
 
-// UserFromContext gets user from the context.
+// UserFromContext gets user from the context. The user may be nil if no authorized header is sent.
 func UserFromContext(ctx context.Context, entClient *ent.Client, firebaseAuth *auth.Client) (*ent.User, error) {
 	authContext, _ := ctx.Value(authContextKey).(*AuthContext)
 	if authContext == nil {
+		// This normally won't happen because the auth context is set in the context.
 		return nil, nil
 	}
 
 	return authContext.User(ctx, entClient, firebaseAuth)
+}
+
+// RequireUserFromContext gets user from the context. If the user is not present, it return unauthenticated error.
+func RequireUserFromContext(ctx context.Context, entClient *ent.Client, firebaseAuth *auth.Client) (*ent.User, error) {
+	user, err := UserFromContext(ctx, entClient, firebaseAuth)
+	if err != nil {
+		return nil, err
+	}
+
+	if user == nil {
+		return nil, ErrUnauthorizedAccess
+	}
+
+	return user, nil
 }
 
 // WithBearerAuth middleware extract the authorization bearer header into a struct that can be used
