7aa01afa669bbe49cf8237d1dd75ba5619afabc1
Sharad Chand <pepsighan@sharadchand.com>
2021-06-08 20:41:33 +0545

    Add a bin to migrate the generated sql

--- bin/migrate/main.go
+++ bin/migrate/main.go
@@ -1,32 +1,29 @@
 package main
 
 import (
-	"context"
+	"database/sql"
 	"log"
 	"os"
 
 	_ "github.com/lib/pq"
-	"github.com/pepsighan/graftini_backend/ent"
 	"github.com/pepsighan/graftini_backend/internal/config"
 )
 
-// Generate the migration manually. Do not automigrate. We can track
-// if any issues exist with the generated SQL.
+// Run the migrations that are generated in the generate directory.
 func main() {
-	client, err := ent.Open("postgres", config.DatabaseURL)
+	client, err := sql.Open("postgres", config.DatabaseURL)
 	if err != nil {
 		log.Fatalf("failed connecting to postgres: %v", err)
 	}
 	defer client.Close()
 
-	f, err := os.Create("./bin/migrate/migration.sql")
+	migrations, err := os.ReadFile("./bin/generate/migration.sql")
 	if err != nil {
-		log.Fatalf("could not create migration file: %v", err)
+		log.Fatalf("could not read migration file: %v", err)
 	}
-	defer f.Close()
 
-	ctx := context.Background()
-	if err := client.Schema.WriteTo(ctx, f); err != nil {
-		log.Fatalf("failed printing schema changes: %v", err)
+	_, err = client.Exec(string(migrations[:]))
+	if err != nil {
+		log.Fatalf("could not migrate sql: %v", err)
 	}
 }
