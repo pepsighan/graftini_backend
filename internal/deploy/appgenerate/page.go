f01a0b8489f931b6946b0c80cb4b838ea34ac112
Sharad Chand <pepsighan@sharadchand.com>
2021-06-25 11:28:19 +0545

    Generate props

--- internal/deploy/appgenerate/page.go
+++ internal/deploy/appgenerate/page.go
@@ -23,31 +23,44 @@ func buildPage(pg *ent.Page) (string, error) {
 	// Make it simple and unique to implement.
 	sb.WriteString(strings.ReplaceAll(pg.ID.String(), "-", ""))
 	sb.WriteString("() {\n")
-	buildPageMarkup(&sb, componentMap)
+
+	if err := buildPageMarkup(&sb, componentMap); err != nil {
+		return "", err
+	}
+
 	sb.WriteString("}")
 	return sb.String(), nil
 }
 
 // buildPageMarkup generates the rendering markup for the page.
-func buildPageMarkup(sb *strings.Builder, componentMap schema.ComponentMap) {
+func buildPageMarkup(sb *strings.Builder, componentMap schema.ComponentMap) error {
 	sb.WriteString("return (<>")
 
 	// Build the markup from the root.
 	root := componentMap["ROOT"]
 	for _, childID := range root.ChildrenNodes {
-		buildSubTreeMarkup(sb, childID, componentMap)
+		err := buildSubTreeMarkup(sb, childID, componentMap)
+		if err != nil {
+			return err
+		}
 	}
 
 	sb.WriteString("</>);")
+	return nil
 }
 
 // buildSubTreeMarkup generates the markup for the component and its children.
-func buildSubTreeMarkup(sb *strings.Builder, componentID string, componentMap schema.ComponentMap) {
+func buildSubTreeMarkup(sb *strings.Builder, componentID string, componentMap schema.ComponentMap) error {
 	comp := componentMap[componentID]
 
 	// Start tag of the component.
 	sb.WriteString("<")
 	sb.WriteString(comp.Type)
+
+	if err := buildProps(sb, &comp); err != nil {
+		return err
+	}
+
 	sb.WriteString(">")
 
 	// Render the children components.
@@ -61,6 +74,28 @@ func buildSubTreeMarkup(sb *strings.Builder, componentID string, componentMap sc
 	sb.WriteString("</")
 	sb.WriteString(comp.Type)
 	sb.WriteString(">")
+
+	return nil
+}
+
+// buildProps generates a series of prop assignments.
+func buildProps(sb *strings.Builder, comp *schema.ComponentNode) error {
+	for k, v := range comp.Props {
+		sb.WriteString(" ")
+		sb.WriteString(k)
+		sb.WriteString("={")
+
+		// The prop may be an object
+		value, err := json.Marshal(v)
+		if err != nil {
+			return err
+		}
+
+		sb.Write(value)
+		sb.WriteString("}")
+	}
+
+	return nil
 }
 
 // parseComponentMap parses the string to a component map.
