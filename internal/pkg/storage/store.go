92d85503258c8c5d7dfb2b10e7462851edebda5c
Sharad Chand <pepsighan@sharadchand.com>
2021-07-09 16:07:54 +0545

    Name the upload object based on the kind

--- internal/pkg/storage/store.go
+++ internal/pkg/storage/store.go
@@ -9,21 +9,27 @@ import (
 	"github.com/google/uuid"
 	"github.com/pepsighan/graftini_backend/internal/backend/config"
 	"github.com/pepsighan/graftini_backend/internal/pkg/ent"
+	"github.com/pepsighan/graftini_backend/internal/pkg/ent/file"
 )
 
 // UploadFile uploads the file to GCP Cloud Storage.
-func UploadFile(ctx context.Context, file io.Reader, storageClient *storage.Client, entClient *ent.Client) (*ent.File, error) {
+func UploadFile(ctx context.Context, reader io.Reader, storageClient *storage.Client, entClient *ent.Client) (*ent.File, error) {
 	fileID := uuid.New()
+	kind := file.KindImage
 
 	bucket := storageClient.Bucket(config.GoogleCloudStorageBucket)
-	object := bucket.Object(fileID.String())
+	object := bucket.Object(uploadFileName(fileID, kind))
 
 	writer := object.NewWriter(ctx)
 	defer writer.Close()
 
-	if _, err := io.Copy(writer, file); err != nil {
+	if _, err := io.Copy(writer, reader); err != nil {
 		return nil, fmt.Errorf("could not upload file: %w", err)
 	}
 
 	return nil, nil
+}
+
+func uploadFileName(fileID uuid.UUID, kind file.Kind) string {
+	return fmt.Sprintf("%v/%v", kind, fileID)
 }
