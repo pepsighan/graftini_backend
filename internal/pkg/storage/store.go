f0a1fe4a15a7c5a290490662012a581e534d0298
Sharad Chand <pepsighan@sharadchand.com>
2021-07-09 16:20:56 +0545

    Check for the mime type

--- internal/pkg/storage/store.go
+++ internal/pkg/storage/store.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 
@@ -12,8 +13,15 @@ import (
 	"github.com/pepsighan/graftini_backend/internal/pkg/ent/file"
 )
 
+// ErrUnsupportedMimeType is returned when a file of unsupported mime type is uploaded.
+var ErrUnsupportedMimeType = errors.New("unsupported_mime_type")
+
 // UploadFile uploads the file to GCP Cloud Storage.
-func UploadFile(ctx context.Context, reader io.Reader, storageClient *storage.Client, entClient *ent.Client) (*ent.File, error) {
+func UploadFile(ctx context.Context, reader io.Reader, mimeType string, storageClient *storage.Client, entClient *ent.Client) (*ent.File, error) {
+	if err := checkIfSupportedImage(mimeType); err != nil {
+		return nil, err
+	}
+
 	fileID := uuid.New()
 	kind := file.KindImage
 
@@ -30,6 +38,25 @@ func UploadFile(ctx context.Context, reader io.Reader, storageClient *storage.Cl
 	return nil, nil
 }
 
+// uploadFileName generates a file name for the object in the GCP bucket.
 func uploadFileName(fileID uuid.UUID, kind file.Kind) string {
 	return fmt.Sprintf("%v/%v", kind, fileID)
+}
+
+var supportedImageTypes = []string{
+	"image/jpeg",
+	"image/png",
+	"image/webp",
+}
+
+// checkIfSupportedImage checks whether the uploaded file is actually an image and
+// a supported one.
+func checkIfSupportedImage(mimeType string) error {
+	for _, supported := range supportedImageTypes {
+		if supported != mimeType {
+			return ErrUnsupportedMimeType
+		}
+	}
+
+	return nil
 }
