6dbfe0f46c5eca0ea114a4c4d0eb00e34a919234
Sharad Chand <pepsighan@sharadchand.com>
2021-07-13 17:10:32 +0545

    Re-enable secure conns

--- internal/backend/deployclient/client.go
+++ internal/backend/deployclient/client.go
@@ -1,55 +1,37 @@
 package deployclient
 
 import (
-	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
-	"time"
 
 	"github.com/pepsighan/graftini_backend/internal/backend/config"
 	"github.com/pepsighan/graftini_backend/internal/deploy/service"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/backoff"
+	"google.golang.org/grpc/credentials"
 )
 
 func grpcConn() (*grpc.ClientConn, error) {
-	// Wait for 100 seconds before the deploy server connects. It won't take as much
-	// time but its just a buffer in case the cloud run spins up a new deploy server
-	// slowly.
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	defer cancel()
-
 	var opts []grpc.DialOption
 
-	// if config.Env.IsLocal() {
-	opts = append(
-		opts,
-		grpc.WithInsecure(),
-		// Wait until a connection is established.
-		grpc.WithBlock(),
-		// Try to reconnect as soon as possible.
-		grpc.WithConnectParams(grpc.ConnectParams{
-			Backoff: backoff.Config{
-				BaseDelay:  1.0 * time.Second,
-				Multiplier: 1.6,
-				Jitter:     0.2,
-				MaxDelay:   10 * time.Second,
-			},
-		}),
-	)
-	// } else {
-	// 	// Enable SSL connections.
-	// 	systemRoots, err := x509.SystemCertPool()
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	cred := credentials.NewTLS(&tls.Config{
-	// 		RootCAs: systemRoots,
-	// 	})
-	// 	opts = append(opts, grpc.WithTransportCredentials(cred))
-	// }
+	if config.Env.IsLocal() {
+		opts = append(
+			opts,
+			grpc.WithInsecure(),
+		)
+	} else {
+		// Enable SSL connections.
+		systemRoots, err := x509.SystemCertPool()
+		if err != nil {
+			return nil, err
+		}
+		cred := credentials.NewTLS(&tls.Config{
+			RootCAs: systemRoots,
+		})
+		opts = append(opts, grpc.WithTransportCredentials(cred))
+	}
 
-	conn, err := grpc.DialContext(
-		ctx,
+	conn, err := grpc.Dial(
 		config.DeployEndpoint,
 		opts...,
 	)
